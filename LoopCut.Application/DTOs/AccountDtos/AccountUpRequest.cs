ea36c670ad3f71ce04db5739e7b37361dd0995f2
PhuHVN <ngocphu2k4@gmail.com>
2026-03-06 14:42:39 +0700

    feat/uploadImage

--- LoopCut.Application/DTOs/AccountDtos/AccountUpRequest.cs
+++ LoopCut.Application/DTOs/AccountDtos/AccountUpRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace LoopCut.Application.DTOs.AccountDtos
     public class AccountUpRequest
     {   
         public string FullName { get; set; } = string.Empty;
+        public IFormFile? AvatarUrl { get; set; }
         public string Address { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
     }
